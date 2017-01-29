6a34123c5035526f8fd27ccab428e9e95a859be1
Russell G Stewart <Russell@russells-macbook-pro.local>
2017-01-29 08:32:25 -0700

    Initial Commit

---
+++ PokieDex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokieDex3
+//
+//  Created by Russell G Stewart on 2017-01-29.
+//  Copyright © 2017 App Whole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
